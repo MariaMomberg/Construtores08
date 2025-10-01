fe8b30948031ce6bc953fe2febb113a11d56ef1b
MariaMomberg <malumomberg2307@gmail.com>
2025-10-01 09:18:29 -0300

    Update ConsultaMain.java

--- ConsultaMain.java
+++ ConsultaMain.java
@@ -1,20 +1,17 @@
 package Aula8Construtores;
 
-import java.util.Date;
-
 public class ConsultaMain {
 	public static void main(String[] args) {
 
-		Date hoje = new Date();
+		String hoje = new String();
 
 		Consulta consulta1 = new Consulta(hoje);
-		Consulta consulta2 = new Consulta("Maria", "Dr. Guilherme");
+		Consulta consulta2 = new Consulta("Maria", "Dr. Gustavo");
 		Consulta consulta3 = new Consulta(hoje, "Murillo", "Dra. Nayara");
 
-		System.out.println("Consulta 1 - Data: " + consulta1.getData());
-		System.out.println(
-				"Consulta 2 - Paciente: " + consulta2.getNomePcente() + " | Dentista: " + consulta2.getNomeDents());
-		System.out.println("Consulta 3 - Data: " + consulta3.getData() + " | Paciente: " + consulta3.getNomePcente()
+		System.out.println("Consulta 1 - Data: 01/10/2025 " + consulta1.getData());
+		System.out.println("Consulta 2 - Paciente: " + consulta2.getNomePcente() + " | Dentista: " + consulta2.getNomeDents());
+		System.out.println("Consulta 3 - Data: 01/10/2025 " + consulta3.getData() + " | Paciente: " + consulta3.getNomePcente()
 				+ " | Dentista: " + consulta3.getNomeDents());
 	}
 }
